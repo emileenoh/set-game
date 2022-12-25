26bd0d0f50aaa09dcd02b4db82c57f1fbdabe850
Emilee Noh <30706216+emileenoh@users.noreply.github.com>
2022-12-24 20:58:42 -0600

    Render stripes, or fill opacity (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,25 @@ import {Count, Shading, Shape, Color} from "./Card";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>The game of SET</h1>
+        <h1>The Game of SET</h1>
         <div style={{textAlign: 'left'}}>
           <strong>Things to work on:</strong>
           <ul>
-            <li>Create a card class</li>
             <li>Write a function to say "is this a set or not?"</li>
             <li>Write a function to generate every card</li>
             <li>Testing</li>
-            <li>UI (Emilee)</li>
-            <li>Set up github repo and commit this (Emilee)</li>
           </ul>
           <p>Goals: learning TS (interfaces, types, react), learning the "backend of the frontend" (hooks, side effects, etc)</p>
         </div>
-        <div className="card-grid">
-          <SetCard count={Count.One} shape={Shape.Oval} color={Color.Purple} shading={Shading.Empty}></SetCard>
+      <div className="card-grid">
+          <SetCard count={Count.Three} shape={Shape.Squiggle} color={Color.Purple} shading={Shading.Striped}></SetCard>
+          <SetCard count={Count.Three} shape={Shape.Squiggle} color={Color.Purple} shading={Shading.Empty}></SetCard>
+          <SetCard count={Count.Three} shape={Shape.Squiggle} color={Color.Purple} shading={Shading.Solid}></SetCard>
+          <SetCard count={Count.Three} shape={Shape.Oval} color={Color.Green} shading={Shading.Striped}></SetCard>
           <SetCard count={Count.Two} shape={Shape.Oval} color={Color.Red} shading={Shading.Solid}></SetCard>
-          <SetCard count={Count.Two} shape={Shape.Oval} color={Color.Green} shading={Shading.Striped}></SetCard>
-        </div>
-      </header>
+          <SetCard count={Count.Two} shape={Shape.Diamond} color={Color.Red} shading={Shading.Striped}></SetCard>
+          <SetCard count={Count.One} shape={Shape.Diamond} color={Color.Red} shading={Shading.Striped}></SetCard>
+      </div>
     </div>
   );
 }
--- src/SVG.tsx
+++ src/SVG.tsx
@@ -1,4 +1,5 @@
-import {Count, Shading, Shape, Color} from "./Card";
+import {Color, Shading, Shape} from "./Card";
+
 interface SVGProps  {
     shape: Shape;
     color: Color;
@@ -6,45 +7,54 @@ interface SVGProps  {
 }
 
 const SVG = ({shape, color, shading} :SVGProps) => {
-    const pattern = (
+    const renderDefs = (color: Color) => (
         <defs>
-            <pattern id="pattern"
-                     width="8" height="10"
-                     patternUnits="userSpaceOnUse"
-                     patternTransform="rotate(45 50 50)">
-                <line stroke-width="4px" y2="10"/>
+            <pattern stroke={color} id={`pattern-${color}`} patternUnits="userSpaceOnUse" width="5" height="10">
+                <line x1="0" y="0" x2="0" y2="10"/>
             </pattern>
-        </defs>);
+        </defs>
+    );
     function getFill() :string {
-        if(shading == Shading.Empty)
+        if(shading === Shading.Empty)
             return 'transparent';
-        if(shading == Shading.Solid)
-            return color;
-        return 'use(#pattern)';
-    };
+        return color;
+    }
+
+    function getFillOpacity(): string {
+        return (shading === Shading.Striped ? '80%' : '100%');
+    }
 
     const fill = getFill();
     return (
         <>
             {shape === Shape.Oval &&
-
-                <svg id="oval" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg">
-                    <ellipse stroke={color} strokeWidth={5} fill={fill} cx="100" cy="50" rx="80" ry="40" />
+                <svg
+                    id="oval"
+                    width="100"
+                    height="200"
+                    viewBox="0 0 50 100"
+                    xmlns="http://www.w3.org/2000/svg"
+                    stroke={color}
+                    strokeWidth={2}
+                >
+                    {renderDefs(color)}
+                    <ellipse fill={getFill()} cx="50%" cy="50%" rx="20" ry="26" />
                 </svg>
             }
             {shape === Shape.Diamond &&
-                <svg id="diamond" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 223.646 223.646" xmlSpace="preserve">
+                <svg
+                    id="diamond"
+                    width="100"
+                    height="200"
+                    xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="10 0 210 220" xmlSpace="preserve">
+                    {renderDefs(color)}
                     <polygon fill={fill} stroke={color} strokeWidth={5} points="111.823,0 16.622,111.823 111.823,223.646 207.025,111.823 "/>
                 </svg>
             }
             {shape === Shape.Squiggle &&
-                <svg id="heart" xmlns="http://www.w3.org/2000/svg" height="200px" width="200px" viewBox="0 0 612 792">
-                    <path fill={fill} stroke={color} strokeWidth={5} d="M611.721,288.299c-2.258-42.176-20.114-81.782-50.287-111.524c-30.557-30.119-70.43-46.708-112.27-46.708
-                c-62.267,0-107.396,49.233-131.641,75.684c-3.743,4.085-8.13,8.87-11.183,11.79c-2.444-2.529-5.756-6.3-8.803-9.768
-                c-22.142-25.222-68.223-77.704-134.688-77.704c-41.84,0-81.711,16.588-112.268,46.708C20.408,206.517,2.547,246.121,0.29,288.299
-                c-2.248,42.107,8.521,78.746,34.92,118.803c20.888,31.701,75.961,93.605,133.927,150.543c29.856,29.326,57.336,54.18,79.466,71.873
-                c35.936,28.729,49.7,32.413,57.674,32.413c7.476,0,21.614-3.352,57.895-32.332c22.079-17.637,49.463-42.451,79.194-71.76
-                c57.445-56.63,112.318-118.617,133.443-150.743C594.576,380.072,614.6,342.151,611.721,288.299z"/>
+                <svg width="100" height="200" viewBox="-20 -40 100 200" xmlns="http://www.w3.org/2000/svg">
+                    {renderDefs(color)}
+                    <rect width="70" height="120" fill={fill} fillOpacity={getFillOpacity()} stroke={color} strokeWidth={5}/>
                 </svg>
             }
         </>
